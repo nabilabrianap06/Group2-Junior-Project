a9836466c0d914c47d2d055399a4c56e679e31e9
Devzka <zukananda7@gmail.com>
2026-09-17 11:14:31 +0700

    Enhance RegisteredUser with new attributes and methods
    Added properties and methods for AI usage, carbon footprint, AI model comparison, and recommendations.

--- src/AIEnvironmentalTracker.Domain/Models/RegisteredUser.cs
+++ src/AIEnvironmentalTracker.Domain/Models/RegisteredUser.cs
@@ -2,10 +2,39 @@ namespace AIEnvironmentalTracker.Domain.Models;
 
 public class RegisteredUser : User
 {
+    // Attributes matching UML
     public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
 
-    public void RecordAIUsage() { /* TODO */ }
-    public void ViewCarbonFootprint() { /* TODO */ }
-    public void CompareAIModels() { /* TODO */ }
-    public void ViewRecommendations() { /* TODO */ }
+    // 1-to-Many Navigations based on UML associations
+    public ICollection<AIUsageLog> UsageLogs { get; set; } = new List<AIUsageLog>();
+    public ICollection<CarbonFootprintReport> CarbonFootprintReports { get; set; } = new List<CarbonFootprintReport>();
+    public ICollection<RecommendationEngine> Recommendations { get; set; } = new List<RecommendationEngine>();
+    public ICollection<AIComparison> AIComparisons { get; set; } = new List<AIComparison>();
+
+    // Operations matching UML
+    public void RecordAIUsage()
+    {
+        var log = new AIUsageLog();
+        log.ScanDeviceActivity();
+        log.CreateUsageLog();
+        UsageLogs.Add(log);
+    }
+
+    public CarbonFootprintReport ViewCarbonFootprint()
+    {
+        return CarbonFootprintReports.LastOrDefault() ?? new CarbonFootprintReport();
+    }
+
+    public AIComparison CompareAIModels()
+    {
+        var comparison = new AIComparison();
+        comparison.CompareModels();
+        AIComparisons.Add(comparison);
+        return comparison;
+    }
+
+    public IEnumerable<RecommendationEngine> ViewRecommendations()
+    {
+        return Recommendations;
+    }
 }
